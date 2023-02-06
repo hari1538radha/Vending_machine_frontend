1112142e82ddcec1c1a2befe8353de52eca375c4
hari1538radha <hari.r2020ece@sece.ac.in>
2023-02-06 15:24:04 +0530

    feat :integrayion with backend completed

---
+++ src/Store/Slice/Login.js
@@ -0,0 +1,29 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { axio } from "../../Components/Config/Config.js";
+export const Login = createAsyncThunk("login", async (data) => {
+  console.log(data);
+  return axio.post(`/api/login`, data);
+});
+
+const LoginData = createSlice({
+  name: "login",
+  initialState: {
+    LoginData: [],
+    LoginLoading: true,
+  },
+  extraReducers: {
+    [Login.pending]: (state, action) => {
+      state.LoginLoading = true;
+    },
+    [Login.fulfilled]: (state, action) => {
+      state.LoginLoading = false;
+      state.LoginData = action.payload;
+    },
+    [Login.rejected]: (state, action) => {
+      state.LoginLoading = true;
+    },
+  },
+});
+
+const LoginDetails = LoginData.reducer;
+export default LoginDetails;
