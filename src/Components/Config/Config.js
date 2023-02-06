738762aa0b3145d6707b183e22fd9be8bdc7ef44
hari1538radha <hari.r2020ece@sece.ac.in>
2023-02-06 15:53:27 +0530

    fix : code formatted

--- src/Components/Config/Config.js
+++ src/Components/Config/Config.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 export const axio = axios.create({
   baseURL: "https://nice-pink-chinchilla-belt.cyclic.app",
-  //https://mern-acadamy-app.herokuapp.com
   headers: {
     "X-Custom-Header": "foobar",
     Accept: "application/json",
